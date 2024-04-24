6a4da8f848a20b51f4655122281f0d3cfae985e0
ludaware.cc <150091058+y2external@users.noreply.github.com>
2024-04-24 18:54:22 +1200

    streamable.lua

---
+++ streamable.lua
@@ -0,0 +1,19 @@
+--[[
+/$$                 /$$               /$$                    
+| $$                | $$              | $$                    
+| $$ /$$   /$$  /$$$$$$$  /$$$$$$     | $$ /$$   /$$  /$$$$$$ 
+| $$| $$  | $$ /$$__  $$ |____  $$    | $$| $$  | $$ |____  $$
+| $$| $$  | $$| $$  | $$  /$$$$$$$    | $$| $$  | $$  /$$$$$$$
+| $$| $$  | $$| $$  | $$ /$$__  $$    | $$| $$  | $$ /$$__  $$
+| $$|  $$$$$$/|  $$$$$$$|  $$$$$$$ /$$| $$|  $$$$$$/|  $$$$$$$
+|__/ \______/  \_______/ \_______/|__/|__/ \______/  \_______/
+  ]]                                                            
+                                                              
+game.StarterGui:SetCore("SendNotification", {
+    Title = "luda.lua";
+    Text = "welcome to luda.lua best new streamable... enjoy :)";
+
+})
+
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/y2external/luda.lua/main/streamable.lua", true))()
